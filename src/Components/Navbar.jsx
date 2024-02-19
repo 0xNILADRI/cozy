78cd828aa2e58b64cd7145698bec1017647932c6
Niladri Ghosh <niladri1406@gmail.com>
2024-02-19 23:56:26 +0530

    feat: optimized switching theme - rendering style to other icons

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,6 +8,12 @@ function Navbar() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLightMode, setIsLightMode] = useState(false);
 
+  const switchStyle = {
+    filter: isLightMode
+      ? ""
+      : "invert(100%) sepia(100%) saturate(15%) hue-rotate(208deg) brightness(104%) contrast(102%)",
+  };
+
   return (
     <nav className="navBar">
       <div>
@@ -16,15 +22,11 @@ function Navbar() {
           src={isPlaying ? pauseButton : playButton}
           alt="media control button"
           onClick={() => setIsPlaying(!isPlaying)}
-          style={{
-            filter: isLightMode
-              ? ""
-              : "invert(100%) sepia(100%) saturate(15%) hue-rotate(208deg) brightness(104%) contrast(102%)",
-          }}
+          style={switchStyle}
         />
       </div>
       <div>
-        <h1>COZY</h1>
+        <h1>Cozy</h1>
       </div>
       <div>
         <img
@@ -32,11 +34,7 @@ function Navbar() {
           src={isLightMode ? darkModeButton : lightModeButton}
           alt="theme change button for UI"
           onClick={() => setIsLightMode(!isLightMode)}
-          style={{
-            filter: isLightMode
-              ? ""
-              : "invert(100%) sepia(100%) saturate(15%) hue-rotate(208deg) brightness(104%) contrast(102%)",
-          }}
+          style={switchStyle}
         />
       </div>
     </nav>
